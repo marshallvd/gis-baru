6d922f0f595b62d7e196295106cc75566a8f4955
marshallvd <mademarshall97@gmail.com>
2024-03-20 22:53:58 +0800

    first commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,74 @@
+<footer class="bg-white rounded shadow p-5 mb-4 mt-4">
+    <div class="row align-items-center">
+        
+        <div class="col-md-4 text-md-start">
+            <p class="mb-0"><a class="text-primary fw-normal">2105551093</a></p>
+        </div>
+        
+
+        <div class="col-md-4 mb-4 mb-md-0 text-center">
+            <!-- List -->
+            <ul class="list-inline mb-0">
+                <li class="list-inline-item px-2">
+                    <a href="https://www.linkedin.com/in/marshallvd/" target="_blank">
+                        <img class="avatar rounded-circle" alt="LinkedIn" src="{{ asset('img/icons/linkedin.png') }}">
+                    </a>
+                </li>
+                <li class="list-inline-item px-2">
+                    <a href="https://www.instagram.com/marshallvd/?hl=en" target="_blank">
+                        <img class="avatar rounded-circle" alt="Instagram" src="{{ asset('img/icons/instagram.png') }}">
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="col-md-4 mb-4 mb-md-0 text-md-end">
+            <p class="mb-0">2024 - <span class="current-year"></span> Sistem Informasi Geografis</p>
+        </div>
+        
+    </div>
+</footer>
+
+
+</main>
+
+<!-- Core -->
+<script src="{{ asset('vendor/@popperjs/core/dist/umd/popper.min.js') }}"></script>
+<script src="{{ asset('vendor/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+
+<!-- Vendor JS -->
+<script src="{{ asset('vendor/onscreen/dist/on-screen.umd.min.js') }}"></script>
+
+<!-- Slider -->
+<script src="{{ asset('vendor/nouislider/dist/nouislider.min.js') }}"></script>
+
+<!-- Smooth scroll -->
+<script src="{{ asset('vendor/smooth-scroll/dist/smooth-scroll.polyfills.min.js') }}"></script>
+
+<!-- Charts -->
+
+
+<!-- Datepicker -->
+<script src="{{ asset('vendor/vanillajs-datepicker/dist/js/datepicker.min.js') }}"></script>
+
+<!-- Sweet Alerts 2 -->
+<script src="{{ asset('vendor/sweetalert2/dist/sweetalert2.all.min.js') }}"></script>
+
+<!-- Moment JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.27.0/moment.min.js"></script>
+
+<!-- Vanilla JS Datepicker -->
+<script src="{{ asset('vendor/vanillajs-datepicker/dist/js/datepicker.min.js') }}"></script>
+
+<!-- Notyf -->
+<script src="{{ asset('vendor/notyf/notyf.min.js') }}"></script>
+
+<!-- Simplebar -->
+<script src="{{ asset('vendor/simplebar/dist/simplebar.min.js') }}"></script>
+
+<!-- Github buttons -->
+<script async defer src="https://buttons.github.io/buttons.js"></script>
+
+@stack('javascript')
+<!-- Volt JS -->
+{{-- <script src="{{ asset('volt/hmtl&css/assets/js/volt.js') }}"></script> --}}
+
